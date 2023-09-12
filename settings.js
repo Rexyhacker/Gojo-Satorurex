b662cbe3cb65f14477f3d61b3e215bc0701f7b62
Rexyhacker <108757218+Rexyhacker@users.noreply.github.com>
2023-09-12 23:32:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,14 +19,14 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['2347044986765']
+global.premium = ['2247044986765']
+global.ownernomer = '2347044986765'
+global.ownername = '🐦Star Boy RICHIE'
+global.botname = 'BOT-DEY-ON-COLOS'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'Nigeria'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
